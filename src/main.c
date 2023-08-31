db2b8a10a669bd34b9e42f8237f640fcace5e9f8
Diego Camilo Silva Gomez <dsilva-g@c4r2s5.42urduliz.com>
2023-08-31 12:57:12 +0200

    Main validating argv

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:49:31 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/08/30 01:41:06 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/08/31 12:55:07 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,60 @@
 
 void	error_handling(char *s)
 {
-	ft_printf("%s\n", s);
-	exit(1);
+	if (s == NULL)
+		exit(1);
+	else
+	{
+		ft_printf("%s\n", s);
+		exit(1);
+	}
 }
 
-void	free_2d_ptr(char **s, int size)
+void	free_2d_str(char **s, int size)
 {
 	int	idx;
 
 	idx = 0;
-	while (idx < size)
+	while (s[idx])
 	{
 		free(s[idx]);
 		idx++;
 	}
 	free(s);
 }
-/*
+
 int	validate_chr(char *s)
 {
 	unsigned int	idx;
 
 	idx = 0;
+	if (ft_isdigit(s[idx]) || s[idx] == '-' || s[idx] == '+')
+		idx++;
+	else
+		return (0);
 	while (s[idx])
 	{
-		if (ft_isdigit(s[idx]) || ((s[idx] = '-' || s[idx] '+')))
-		{
-
-		}
+		if (!ft_isdigit(s[idx]))
+			return (0);
+		idx++;
 	}
+	return (1);
 }
-*/
-void	validate_argv(char **sn, int size)
-{
-	int	i;
-	int	j;
 
-	i = 0;
-	while (i < size)
+void	validate_av(char **str_num)
+//void	validate_av(char **str_num, int size)
+{
+	int	idx;
+
+	idx = 0;
+	while (str_num[idx])
 	{
-		j = 0;
-		while (sn[i][j])
+		if (validate_chr(str_num[idx]) == 0)
 		{
-			if ((!ft_isdigit(sn[i][j]) || sn[i][j] != '+' || sn[i][j] != '-') \
-					|| (ft_isdigit(sn[i][j]) && sn[i][j + 1] && \
-						(sn[i][j + 1] == '-' || sn[i][j + 1] == '+' )))
-			{
-				free_2d_ptr(sn, size);
-				error_handling("ERROR : argv must be numbers");
-				j++;
-			}
+			free_2d_str(str_num);
+			error_handling("ERROR : argv must be numbers");
 		}
-		i++;
+		idx++;
 	}
 }
 
@@ -79,11 +81,42 @@ void	validate_args(int ac, char **av)
 		validate_argv(av + 1, ac - 1);
 }
 
+int	validate_str_av(char *s)
+{
+	unsigned int	idx;
+
+	if (!s || !*s)
+		return (0);
+	idx = 0;
+	while (s[idx])
+	{
+		if (!ft_isdigit(s[idx]) && s[idx] != '+' && s[idx] != '-' && \
+				s[idx] != ' ')
+			return (0);
+		idx++;
+	}
+	return (1);
+}
+
 int	main(int ac, char *av[])
 {
+	char	**str_num;
 	t_stack	*stk;
 
-	validate_args(ac, av);
+	if (ac == 1)
+		error_handling(NULL);
+	else if (ac == 2)
+	{
+		if (validate_str_av(av[1]) == 0)
+			error_handling("ERROR : wrong chararcter");
+		str_num = split(av[1] = ' ')
+		//validate_av(str_num, ac - 1);
+		validate_av(str_num);
+	}
+	else
+		str_num = av + 1;
+		//validate_av(str_num, ac - 1);
+		validate_av(str_num);
 	stk = (t_stack *)ft_calloc(sizeof(t_stack), 1);
 	if(!stk)
 		error_handling("ERROR : malloc");
