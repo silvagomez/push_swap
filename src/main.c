6beb802e75b097f0cc8a154789684634e13694de
Diego Camilo Silva Gomez <dsilva-g@c4r2s5.42urduliz.com>
2023-08-31 13:59:40 +0200

    Update, test in linux

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:49:31 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/08/31 12:55:07 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/08/31 13:43:20 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 void	error_handling(char *s)
 {
 	if (s == NULL)
+	{
 		exit(1);
+	}
 	else
 	{
 		ft_printf("%s\n", s);
@@ -23,7 +25,7 @@ void	error_handling(char *s)
 	}
 }
 
-void	free_2d_str(char **s, int size)
+void	free_2d_str(char ***s)
 {
 	int	idx;
 
@@ -62,15 +64,18 @@ void	validate_av(char **str_num)
 	idx = 0;
 	while (str_num[idx])
 	{
+		ft_printf("antes %s\n", str_num[idx]);
 		if (validate_chr(str_num[idx]) == 0)
 		{
-			free_2d_str(str_num);
+			//return (0);
+			//free_2d_str(&str_num);
 			error_handling("ERROR : argv must be numbers");
 		}
 		idx++;
 	}
+	//return (1);
 }
-
+/*
 void	validate_args(int ac, char **av)
 {
 	if (ac == 1 || (ac == 2 && !av[1][0]))
@@ -80,22 +85,21 @@ void	validate_args(int ac, char **av)
 	else
 		validate_argv(av + 1, ac - 1);
 }
-
-int	validate_str_av(char *s)
+*/
+void	validate_str_av(char *s)
 {
 	unsigned int	idx;
 
 	if (!s || !*s)
-		return (0);
+		error_handling("ERROR : wrong chararcter");
 	idx = 0;
 	while (s[idx])
 	{
 		if (!ft_isdigit(s[idx]) && s[idx] != '+' && s[idx] != '-' && \
 				s[idx] != ' ')
-			return (0);
+			error_handling("ERROR : wrong chararcter");
 		idx++;
 	}
-	return (1);
 }
 
 int	main(int ac, char *av[])
@@ -107,16 +111,17 @@ int	main(int ac, char *av[])
 		error_handling(NULL);
 	else if (ac == 2)
 	{
-		if (validate_str_av(av[1]) == 0)
-			error_handling("ERROR : wrong chararcter");
-		str_num = split(av[1] = ' ')
-		//validate_av(str_num, ac - 1);
+		validate_str_av(av[1]);
+		str_num = NULL;
+		str_num = ft_split(av[1], ' ');
 		validate_av(str_num);
 	}
 	else
+	{
+		str_num = NULL;
 		str_num = av + 1;
-		//validate_av(str_num, ac - 1);
 		validate_av(str_num);
+	}
 	stk = (t_stack *)ft_calloc(sizeof(t_stack), 1);
 	if(!stk)
 		error_handling("ERROR : malloc");
