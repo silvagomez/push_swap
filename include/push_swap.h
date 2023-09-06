a0abaed7c5be66c132831b555d5f582fd5015610
Diego Camilo Silva Gomez <dsilva-g@c4r1s4.42urduliz.com>
2023-09-06 13:30:14 +0200

    Update .h, new functions

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,14 +6,14 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:50:11 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/09/06 00:33:46 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/09/06 12:17:46 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-//Allowed libraries
+// Allowed libraries
 # include "../libft/libft.h"
 
 typedef struct s_stack_node
@@ -35,6 +35,20 @@ typedef struct	s_stack
 
 }				t_stack;
 
+// Validate functions
+void	validate_str_av(char *s);
+void	validate_av(char **str_num, int event);
+int		validate_chr(char *s);
+void	validate_is_duplicate(char **str_num, int size,  int event);
+int		is_duplicate(long *nums, int size);
+
 long	ft_atol(const char *str);
+
+// Error functions
+void	error_terminate(char *str_error);
+void	error_validate(char **s, int event);
+
+// Free memory functions
+void	free_2d_str(char **s);
 
 #endif
