e736376c5c19136d7cfdf78fe37eb3ab6154b108
silvagomez <silva.camilo@gmail.com>
2023-09-15 23:07:58 +0200

    Creating sort functions

--- src/sort.c
+++ src/sort.c
@@ -1,14 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_stack.c                                       :+:      :+:    :+:   */
+/*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/31 12:28:56 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/09/15 23:03:39 by dsilva-g         ###   ########.fr       */
+/*   Created: 2023/08/31 12:31:05 by dsilva-g          #+#    #+#             */
+/*   Updated: 2023/09/15 22:59:35 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	sort_three(t_stack **a)
+{
+	t_stack	*highest_node;
+
+	highest_node = stack_get_highest_node(*a);
+	if (*a == highest_node)
+		ra(a);
+	else if ((*a)->next == highest_node)
+		rra(a);
+	if ((*a)->num > (*a)->next->num)
+		sa(a);
+}
+
+void	sort_stack(t_stack **a, t_stack **b)
+{
+	(void)a;
+	(void)b;
+	ft_printf("HOLA");
+
+}
