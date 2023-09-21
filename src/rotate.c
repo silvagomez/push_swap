8b7dfeeedbe6c2750bae5a964dcb94fff97f2a64
Diego Camilo Silva Gomez <dsilva-g@c3r2s6.42urduliz.com>
2023-09-21 13:25:36 +0200

    fix: error compiler

--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 10:52:21 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/09/16 15:58:12 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/09/21 11:11:12 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,20 +49,20 @@ void	rr(t_stack **a, t_stack **b)
  * The aim of this function is that the first/head node in "a" or "b" stack
  * should be the stack_node.
  */
-void	rotate_stack(t_stack **stack, t_stack stack_node, char c)
+void	rotate_stack(t_stack **stack, t_stack *stack_node, char c)
 {
 	while (*stack != stack_node)
 	{
-		if (c = 'a')
+		if (c == 'a')
 		{
-			if (stack_node->1st_half == 1)
+			if (stack_node->one_half == 1)
 				ra(stack);
 			else
 				rra(stack);
 		}
 		else
 		{
-			if (stack_node->1st_half == 1)
+			if (stack_node->one_half == 1)
 				rb(stack);
 			else
 				rrb(stack);
--- src/rotate_optimize.c
+++ src/rotate_optimize.c
@@ -6,21 +6,21 @@
 /*   By: dsilva-g <dsilva-g@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 17:09:18 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/09/19 16:23:25 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/09/21 11:29:27 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rotate_optimize(t_satck **a, t_stack_b **b, t_stack cheapest_node, \
-		site_t fst_half)
+void	rotate_optimize(t_stack **a, t_stack **b, t_stack *cheapest_node, \
+		size_t one_half)
 {
 	while (*a != cheapest_node->target && *b != cheapest_node)
 	{
-		if (fst_half == 1)
+		if (one_half == 1)
 			rr(a, b);
-		else if (fst_half == 0)
-			rrr(a,b)
+		else if (one_half == 0)
+			rrr(a,b);
 	}
 	stack_set_idx(*a);
 	stack_set_idx(*b);
