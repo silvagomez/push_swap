1de481116e6f2521a38f2e5b4e51bd7c2c7e97f0
Diego Camilo Silva Gomez <dsilva-g@c4r1s4.42urduliz.com>
2023-09-25 11:19:05 +0200

    fix: idea for cheapest node, need test linux

--- src/stack_set_move_cost.c
+++ src/stack_set_move_cost.c
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 23:18:45 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/09/22 17:16:23 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/09/25 11:16:58 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 /*
  * Like vim calculate the relaitve number for move_cost in stack "b"
  */
+/*
 void	stack_set_move_cost(t_stack *a, t_stack *b)
 {
 	size_t	size_a;
@@ -34,6 +35,34 @@ void	stack_set_move_cost(t_stack *a, t_stack *b)
 			b->move_cost += b->target->idx;
 		else
 			b->move_cost += size_a - b->target->idx;
+		b = b->next;
+	}
+}
+*/
+
+void	stack_set_move_cost(t_stack *a, t_stack *b)
+{
+	size_t	size_a;
+	size_t	size_b;
+
+	//if (!a | !b)
+	//	return ;
+	size_a = stack_size(a);
+	size_b = stack_size(b);
+	while (b)
+	{
+		//ft_printf("SET move cost\n");
+		b->move_cost = b->idx;
+		if (b->one_half == 0)
+			b->move_cost = size_b - b->idx;
+		if (b->target->one_half == 1 && b->one_half == 1)
+			b->move_cost += b->move_cost;
+		else if (b->target->one_half == 0 && b->one_half == 1)
+			b->move_cost += b->target->idx;	
+		else if (b->target->one_half == 1 && b->one_half == 0)
+			b->move_cost += size_a - b->target->idx;
+		else if (b->target->one_half == 0 && b-one_half == 0)
+			b->move_cost += b->target->idx;	
 		b = b->next;
 	}
 }
